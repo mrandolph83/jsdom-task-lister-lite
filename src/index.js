4438c66e5925ea8bd78629da3c58d93080e5b1b1
mrandolph83 <mrandolph83@gmail.com>
2021-07-16 16:55:54 -0400

    Assignment COmplete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+ 
+  // const main = document.querySelector("#main-content")
+  // console.log(main)
+
+ const taskForm = document.querySelector("#create-task-form")
+
+ const taskList = document.querySelector("#tasks")
+
+ taskForm.addEventListener("submit", function(e) {
+   e.preventDefault()
+
+   const newTask = document.querySelector("#new-task-description").value
+
+  taskList.innerHTML += `<li> ${newTask} <button data-action="delete">X</button></li>`
+
+  taskForm.reset()
+
+   
+ })
+
+ taskList.addEventListener("click", function(e) {
+   if (e.target.dataset.action === "delete"){
+     e.target.parentElement.remove()
+   }
+
+ })
+
